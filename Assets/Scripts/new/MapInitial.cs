76def0cda8c53734d1c59194c8ddbdfef1148e24
wushengzhong2 <wuszwangyi@163.com>
2018-10-08 18:54:49 +0800

    feat: 添加图片资源

---
+++ Assets/Scripts/new/MapInitial.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 关卡初始化
+/// </summary>
+public class MapInitial: MonoBehaviour {
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
