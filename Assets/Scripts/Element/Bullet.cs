10b3092847a35f98ac29e95d5453bce3eec73cbc
wushengzhong2 <wuszwangyi@163.com>
2018-10-12 09:24:29 +0800

    feat: 初步了解动画的实现

---
+++ Assets/Scripts/Element/Bullet.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet: MonoBehaviour {
+
+    //==========================================================================
+    // Public Properties
+    //==========================================================================
+    /// <summary>
+    /// The type 0=>player 1=>enemy.
+    /// </summary>
+    public int type;
+
+    //==========================================================================
+    // Private Properties
+    //==========================================================================
+    /// <summary>
+    /// The speed.
+    /// </summary>
+    private float speed = 8f;
+    /// <summary>
+    /// The rb.
+    /// </summary>
+    private Rigidbody2D rb;
+    /// <summary>
+    /// The small explosion.
+    /// </summary>
+    private GameObject smallExplosion, bigExplosion;
+
+    //==========================================================================
+    // Private Properties
+    //==========================================================================
+    // Use this for initialization
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        Vector3 v3 = transform.up.normalized;
+        rb.velocity = v3 * speed;
+
+        smallExplosion = (GameObject)Resources.Load("Prefabs/Elemet/Explosion/SmallExplosion");
+        bigExplosion   = (GameObject)Resources.Load("Prefabs/Elemet/Explosion/BigExplosion");
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    //==========================================================================
+    // Collider Funtions
+    //==========================================================================
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        // 销毁子弹
+        Destroy(this.gameObject);
+        // 更新子弹数
+        switch (collision.tag) {
+            case "brick":
+                Destroy(collision.gameObject);
+                // 触发子弹命中动画
+                Instantiate(smallExplosion, this.transform.position, Quaternion.identity);
+                break;
+            case "grid":
+                if (type == 0 && DataManager.levels[0] == 3) {
+                    //TODO: 判断是否
+                    Destroy(collision.gameObject);
+                }
+                break;
+            case "home":
+                break;
+            case "bound":
+                break;
+            case "enemy":
+                break;
+            case "play":
+                break;
+        }
+    }
+}
--- Assets/Scripts/Game/Start/GTTanks.cs
+++ Assets/Scripts/Game/Start/GTTanks.cs
@@ -1,16 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GTTanks: MonoBehaviour {
 
     //==========================================================================
     // Private Properties
     //==========================================================================
-
+    /// <summary>
+    /// 敌人出现位置
+    /// </summary>
     private Vector3[] enemyBorthPosition = new Vector3[3];
-
+    /// <summary>
+    /// The borth star.
+    /// </summary>
+    private GameObject borthStar;
+    /// <summary>
+    /// The player1.
+    /// </summary>
     private GameObject player1;
+    /// <summary>
+    /// The enemys.
+    /// </summary>
     private GameObject[] enemys = new GameObject[8];
+    Vector3 playerOutPosition = new Vector3(-1.448073f, -6.029788f, 10f);
 
     //==========================================================================
     // Mono Life Cycle
@@ -20,8 +33,11 @@ public class GTTanks: MonoBehaviour {
         enemyBorthPosition[0] = new Vector3(-6f, 6f, 10f);
         enemyBorthPosition[1] = new Vector3(-0f, 6f, 10f);
         enemyBorthPosition[2] = new Vector3(6f, 6f, 10f);
-
+        // ===> 加载 GameObject
         LoadPlayers();
+        //LoadEnemys();
+        // ===> player
+        Instantiate(player1, playerOutPosition, Quaternion.identity);
     }
 
     // Use this for initialization
@@ -39,10 +55,18 @@ public class GTTanks: MonoBehaviour {
     //==========================================================================
 
     private void LoadEnemys() {
-
+        enemys[0] = (GameObject)Resources.Load("Prefabs/Element/Enemy/EnemyA1");
+        enemys[1] = (GameObject)Resources.Load("Prefabs/Element/Enemy/EnemyA2");
+        enemys[2] = (GameObject)Resources.Load("Prefabs/Element/Enemy/EnemyB1");
+        enemys[3] = (GameObject)Resources.Load("Prefabs/Element/Enemy/EnemyB2");
+        enemys[4] = (GameObject)Resources.Load("Prefabs/Element/Enemy/EnemyC1");
+        enemys[5] = (GameObject)Resources.Load("Prefabs/Element/Enemy/EnemyC2");
+        enemys[6] = (GameObject)Resources.Load("Prefabs/Element/Enemy/EnemyD1");
+        enemys[7] = (GameObject)Resources.Load("Prefabs/Element/Enemy/EnemyD2");
     }
 
     private void LoadPlayers() {
-
+        //borthStar = (GameObject)Resources.Load("Prefabs/Element/Borth/BorthStar");
+        player1 = (GameObject)Resources.Load("Prefabs/Element/Player/Player1");
     }
 }
