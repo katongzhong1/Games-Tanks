fa4e5cd41f8c8184bcef70e631bcc6cc85b0cf13
wushengzhong <wushengzhong@gmail.com>
2018-10-13 13:51:32 +0800

    feat: 添加坦克出生动画

---
+++ Assets/Scripts/Element/Borth.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Borth : MonoBehaviour {
+
+    //==========================================================================
+    // Public Property
+    //==========================================================================
+    /// <summary>
+    /// 类型 0 player; 1 enemy;
+    /// </summary>
+    public int type;
+
+    //==========================================================================
+    // Private Properties
+    //==========================================================================
+    /// <summary>
+    /// The player1.
+    /// </summary>
+    private GameObject player1;
+    /// <summary>
+    /// The enemys.
+    /// </summary>
+    private GameObject[] enemys = new GameObject[8];
+
+    //==========================================================================
+    // Mono Life Cycle
+    //==========================================================================
+
+    private void Awake() {
+        // ===> 加载 GameObject
+        LoadPlayers();
+        LoadEnemys();
+    }
+
+    //==========================================================================
+    // Public Functions
+    //==========================================================================
+
+    public void OnAnimCompleted() {
+        Destroy(this.gameObject);
+        if (type == 0) {
+            //==> Enemy
+            int etype = DataManager.enemys[DataManager.stage, DataManager.dieEnemys + DataManager.curEnemys];
+            Instantiate(enemys[etype], transform.position, Quaternion.identity);
+        } else if (type == 1) {
+            //==> Player1
+            Instantiate(player1, transform.position, Quaternion.identity);
+        }
+    }
+
+    //==========================================================================
+    // Public Functions
+    //==========================================================================
+
+    private void LoadEnemys() {
+        enemys[0] = (GameObject)Resources.Load("Prefabs/Element/Enemy/EnemyA1");
+        enemys[1] = (GameObject)Resources.Load("Prefabs/Element/Enemy/EnemyA2");
+        enemys[2] = (GameObject)Resources.Load("Prefabs/Element/Enemy/EnemyB1");
+        enemys[3] = (GameObject)Resources.Load("Prefabs/Element/Enemy/EnemyB2");
+        enemys[4] = (GameObject)Resources.Load("Prefabs/Element/Enemy/EnemyC1");
+        enemys[5] = (GameObject)Resources.Load("Prefabs/Element/Enemy/EnemyC2");
+        enemys[6] = (GameObject)Resources.Load("Prefabs/Element/Enemy/EnemyD1");
+        enemys[7] = (GameObject)Resources.Load("Prefabs/Element/Enemy/EnemyD2");
+    }
+
+    private void LoadPlayers() {
+        player1 = (GameObject)Resources.Load("Prefabs/Element/Player/Player1");
+    }
+}
--- Assets/Scripts/Game/Start/GTTanks.cs
+++ Assets/Scripts/Game/Start/GTTanks.cs
@@ -14,15 +14,10 @@ public class GTTanks: MonoBehaviour {
     /// <summary>
     /// The borth star.
     /// </summary>
-    private GameObject borthStar;
-    /// <summary>
-    /// The player1.
-    /// </summary>
-    private GameObject player1;
+    private GameObject enemyBorth, playerBorth;
     /// <summary>
     /// The enemys.
     /// </summary>
-    private GameObject[] enemys = new GameObject[8];
     Vector3 playerOutPosition = new Vector3(-1.75f, -6f, -1f);
 
     //==========================================================================
@@ -34,10 +29,10 @@ public class GTTanks: MonoBehaviour {
         enemyBorthPosition[1] = new Vector3(-0f, 6f, -1f);
         enemyBorthPosition[2] = new Vector3(6f, 6f, -1f);
         // ===> 加载 GameObject
-        LoadPlayers();
-        LoadEnemys();
+        enemyBorth  = (GameObject)Resources.Load("Prefabs/Element/Borth/EnemyBorth");
+        playerBorth = (GameObject)Resources.Load("Prefabs/Element/Borth/PlayerBorth");
         // ===> player
-        Instantiate(player1, playerOutPosition, Quaternion.identity);
+        Instantiate(playerBorth, playerOutPosition, Quaternion.identity);
         // ===> 开启协程
         StartCoroutine(CreateEnemy());
     }
@@ -56,22 +51,6 @@ public class GTTanks: MonoBehaviour {
     // Private Functions
     //==========================================================================
 
-    private void LoadEnemys() {
-        enemys[0] = (GameObject)Resources.Load("Prefabs/Element/Enemy/EnemyA1");
-        enemys[1] = (GameObject)Resources.Load("Prefabs/Element/Enemy/EnemyA2");
-        enemys[2] = (GameObject)Resources.Load("Prefabs/Element/Enemy/EnemyB1");
-        enemys[3] = (GameObject)Resources.Load("Prefabs/Element/Enemy/EnemyB2");
-        enemys[4] = (GameObject)Resources.Load("Prefabs/Element/Enemy/EnemyC1");
-        enemys[5] = (GameObject)Resources.Load("Prefabs/Element/Enemy/EnemyC2");
-        enemys[6] = (GameObject)Resources.Load("Prefabs/Element/Enemy/EnemyD1");
-        enemys[7] = (GameObject)Resources.Load("Prefabs/Element/Enemy/EnemyD2");
-    }
-
-    private void LoadPlayers() {
-        //borthStar = (GameObject)Resources.Load("Prefabs/Element/Borth/BorthStar");
-        player1 = (GameObject)Resources.Load("Prefabs/Element/Player/Player1");
-    }
-
     IEnumerator CreateEnemy() {
         while (true) {
             if (DataManager.curEnemys < 20) {
@@ -82,8 +61,7 @@ public class GTTanks: MonoBehaviour {
                 num = num > 3 ? 3 : num;
                 for (var i = 0; i < num; i++) {
                     DataManager.curEnemys += 1;
-                    int type = DataManager.enemys[DataManager.stage, DataManager.dieEnemys + DataManager.curEnemys];
-                    Instantiate(enemys[type], enemyBorthPosition[i], Quaternion.identity);
+                    Instantiate(enemyBorth, enemyBorthPosition[i], Quaternion.identity);
                 }
                 yield return new WaitForSeconds(3.0f);
             }
